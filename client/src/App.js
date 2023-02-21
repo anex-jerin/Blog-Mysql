70d8af32a93cad787b3b9fecf57392c29719d2bf
Vikki <anexcruise@gmail.com>
2023-02-21 15:36:39 +0530

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,26 @@
+import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Single from './pages/Single';
+import Write from './pages/Write';
+import Layout from './components/Layout';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/login' element={<Login />} />
+        <Route path='/register' element={<Register />} />
+        <Route element={<Layout />}>
+          <Route path='/' element={<Navigate to={'/home'} replace />} />
+          <Route path='/home' element={<Home />} />
+          <Route path='/single' element={<Single />} />
+          <Route path='/write' element={<Write />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
