f676adaa15b144ce3396423f65c1d399090f0664
Vikki <anexcruise@gmail.com>
2023-02-22 06:57:23 +0530

    register

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,5 +1,27 @@
 const Register = () => {
-  return <div>Register</div>;
+  return (
+    <div className='l-container'>
+      <div className='auth'>
+        <h1>Register</h1>
+        <form>
+          <input type='text' placeholder='Username' />
+          <input type='text' placeholder='Name' />
+          <input type='password' placeholder='Password' />
+          <input type='password' placeholder='Repeat password' />
+          <div className="check">
+            <button>REGISTER</button></div>
+        </form>
+        <div className='p-link'>
+          <a className='blue' href='/login'>
+            Login now
+          </a>
+          <a className='red' href='/reset'>
+            Forgot password ?
+          </a>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Register;
